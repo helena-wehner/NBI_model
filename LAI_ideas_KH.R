338ea37303b56eb8e07d09b18e8c21d15448f86e
helena-wehner <helena-wehner@outlook.de>
2020-12-19 20:17:27 +0100

    ...

--- LAI_ideas_KH.R
+++ LAI_ideas_KH.R
@@ -1,21 +1,167 @@
 ###########################
-###  LAI Loop Structure ###
+###  LAI Values for FHS ###
 ###  Katharina Huchler  ###
-###     2020-10-221     ###
+###  Helena Wehner      ###
+###     2020-12-09      ###
 ###########################
 
-### STEP 1: Buffer Areas ----
+### Load and install (if not installed yet) needed Packages ----
+
+if ( ! require('sp') ) { install.packages('sp');
+  library('sp')}
+if ( ! require("sf") ) { install.packages("sf");
+  library("sf")      }
+if ( ! require("raster") ) { install.packages("raster");
+  library("raster")      }
+if ( ! require("beepr") ) { install.packages("beepr");
+  library("beepr")      }
+
+### STEP 1:  DATA PREPARATION ----
 
 # buffer areas inwards (to make them smaller and avoid including LAI
 # values of surroundings?)
 
+# load fields shapefile
+fields <- st_read('sampledareasshapefile/NBI_FHS_presabs_final_rework.shp')
+
+# just have a short look/check at the shapefile
+plot(fields$geometry)
+
+# create an inner buffer of 5m to avoid side effects of the later used LAI data
+fields_buffer_5 <- st_buffer(fields, unit = 'm', -5)
+plot(fields_buffer_5$geometry) # plot again to check if inner buffer worked
+
+# transform sf dataframe object fields_buffer_5 into shapefile/SpatialPolygonsObject
+fields_buffer_shp <- as(fields_buffer_5, 'Spatial')
+plot(fields_buffer_shp)
+
+# inner buffer of 10m, because the resolution of LAI Images = 10m
+#fields_buffer_10 <- st_buffer(fields, unit = 'm', -10)
+#plot(fields_buffer_10$geometry) # some fields disappear this is why we deside to use an inner buffer
+#of 5m instead of 10m
+
 ### STEP 2: Extract LAI values for every area ----
 
+#### LAI DATA PREPARATION:----
+
+# load LAI Raster files ----
+# UUP File of Sentinel2 LAI Images
+uup <- stack(raster('LAI_Images/UUP_right/August/IMAGE_2020-08-01_33UUP_22_LAI_10m_Id175305793_L2A.tiff'),
+             raster('LAI_Images/UUP_right/August/IMAGE_2020-08-06_33UUP_22_LAI_10m_Id179115446_L2A.tiff'),
+             raster('LAI_Images/UUP_right/August/IMAGE_2020-08-11_33UUP_22_LAI_10m_Id177061733_L2A.tiff'),
+             raster('LAI_Images/UUP_right/July/IMAGE_2020-07-07_33UUP_22_LAI_10m_Id171046190_L2A.tiff'),
+             raster('LAI_Images/UUP_right/July/IMAGE_2020-07-27_33UUP_22_LAI_10m_Id174981989_L2A.tiff'),
+             raster('LAI_Images/UUP_right/June/IMAGE_2020-06-02_33UUP_22_LAI_10m_Id164933579_L2A.tiff'),
+             raster('LAI_Images/UUP_right/June/IMAGE_2020-06-12_33UUP_22_LAI_10m_Id166696852_L2A.tiff'),
+             raster('LAI_Images/UUP_right/June/IMAGE_2020-06-27_33UUP_22_LAI_10m_Id169280403_L2A.tiff'))
+# UQU File of Sentinel2 LAI Images
+uqu <- stack(raster('LAI_Images/UQU_left/August/IMAGE_2020-08-16_32UQU_22_LAI_10m_Id177898068_L2A.tiff'),
+             raster('LAI_Images/UQU_left/July/IMAGE_2020-07-12_32UQU_22_LAI_10m_Id172389242_L2A.tiff'),
+             raster('LAI_Images/UQU_left/July/IMAGE_2020-07-22_32UQU_22_LAI_10m_Id175160268_L2A.tiff'))
+
+# Clip uup and uqu Files to Field Polygons ----
+# --> makes it possible to stack them together
+
+# need to change the projection of uup raster file 
+uup <- projectRaster(uup, uqu)
+beep(sound = 2) # notification 
+# export UUP projected raster --> makes it is easier to redo the script later (saves time)
+#writeRaster(uup,'uup_stack', format = 'GTiff',options="INTERLEAVE=BAND")
+#beep(sound = 2)
+# test: import Raster again
+uup <- stack(raster('uup_stack.tif'))
+
+uup_clip <-mask(crop(uup, fields_buffer_shp), fields_buffer_shp)
+# plot for checking result
+plot(uup_clip$IMAGE_2020.08.01_33UUP_22_LAI_10m_Id175305793_L2A)
+
+uqu_clip <- mask(crop(uqu, fields_buffer_shp),fields_buffer_shp)
+# plot for checking result
+plot(uqu_clip$IMAGE_2020.08.16_32UQU_22_LAI_10m_Id177898068_L2A)
+
+# stack UUP_clip and UQU_clip together
+LAI <- stack(uup_clip, uqu_clip)
+
+# export: LAI clipped Raster Stack (to be on the save side)
+writeRaster(x = LAI, filename = 'LAI.tiff', options="INTERLEAVE=BAND")
+
+# not needed
 # 2.1. convert polygon shapefile of sampled areas to raster resembling LAI data (10x10m resolution)
-## or is there a way to do this with polygons as well?
-## in my opinion, raster would be more easily to handle
+# just clip raster by polygon --> done above
+
+### Masking out Cloud Pixel ----
+### load SCL Images of Sentinel2
+# UUP File of Sentinel2 LAI Images
+uup_scl <- stack(raster('LAI_Images/UUP_right/August/IMAGE_2020-08-01_33UUP_22_SCL_20m_Id175280181_L2A.jp2'),
+             raster('LAI_Images/UUP_right/August/IMAGE_2020-08-06_33UUP_22_SCL_20m_Id179084581_L2A.jp2'),
+             raster('LAI_Images/UUP_right/August/IMAGE_2020-08-11_33UUP_22_SCL_20m_Id177040167_L2A.jp2'),
+             raster('LAI_Images/UUP_right/July/IMAGE_2020-07-07_33UUP_22_SCL_20m_Id170977410_L2A.jp2'),
+             raster('LAI_Images/UUP_right/July/IMAGE_2020-07-27_33UUP_22_SCL_20m_Id174912026_L2A.jp2'),
+             raster('LAI_Images/UUP_right/June/IMAGE_2020-06-02_33UUP_22_SCL_20m_Id164881880_L2A.jp2'),
+             raster('LAI_Images/UUP_right/June/IMAGE_2020-06-12_33UUP_22_SCL_20m_Id166654867_L2A.jp2'),
+             raster('LAI_Images/UUP_right/June/IMAGE_2020-06-27_33UUP_22_SCL_20m_Id169238433_L2A.jp2'))
+# UQU File of Sentinel2 LAI Images
+uqu_scl <- stack(raster('LAI_Images/UQU_left/August/IMAGE_2020-08-16_32UQU_22_SCL_20m_Id177871641_L2A.jp2'),
+             raster('LAI_Images/UQU_left/July/IMAGE_2020-07-12_32UQU_22_SCL_20m_Id172346359_L2A.jp2'),
+             raster('LAI_Images/UQU_left/July/IMAGE_2020-07-22_32UQU_22_SCL_20m_Id175100741_L2A.jp2'))
+
+# Clip uup and uqu Files to Field Polygons ----
+# --> makes it possible to stack them together
+
+# need to change the projection of UUP_scl raster file 
+#uup_scl <- projectRaster(uup_scl, uqu_scl)
+#beep(sound = 2) # notification 
+# export UUP_scl projected raster --> makes it is easier to redo the script later (saves time)
+#writeRaster(uup_scl,'uup_scl_stack', format = 'GTiff',options="INTERLEAVE=BAND")
+#beep(sound = 2)
+# test: import Raster again
+uup_scl <- brick('uup_scl_stack.tif')
+
+uup_scl_clip <-mask(crop(uup_scl, fields_buffer_shp), fields_buffer_shp)
+# plot for checking result
+plot(uup_scl_clip$IMAGE_2020.08.01_33UUP_22_SCL_20m_Id175280181_L2A)
+
+uqu_scl_clip <- mask(crop(uqu_scl, fields_buffer_shp),fields_buffer_shp)
+# plot for checking result
+plot(uqu_scl_clip$IMAGE_2020.08.16_32UQU_22_SCL_20m_Id177871641_L2A)
+
+# stack UUP_clip and UQU_clip together
+SCL <- stack(uup_scl_clip, uqu_scl_clip)
+
+# export: SCL clipped Raster Stack (to be on the save side)
+writeRaster(x = SCL, filename = 'SCL.tiff', options="INTERLEAVE=BAND", overwrite = T)
+
+######################################################################################################
+### START FROM HERE NOW: LOAD THE SAVED LAI AND SCL DATA INSTEAD OF RUNNINGA ALL ABOVE AGAIN !!!!!----
+######################################################################################################
+
+# IMPORT: prepared Data - LAI and SCL Raster stacks
+
+LAI <- stack('LAI.tif')
+SCL <- stack('SCL.tif')
+
+### STEP 2: CLOUD MASKING ----
+
+# create Cloud Mask ----
+# set values of SCL stack (7:11 = related to clouds) to NA
+SCL <- calc(SCL, fun = function(x)
+  {x[x >= 6.99] <- NA;
+  return(x)}
+  )
+
+# check if there are still values above 7 in the SCL raster stack
+summary(getValues(SCL))
+plot(SCL$SCL.11)
+
+# remove from LAI raster stack all values that are NA in SCL raster stack
+LAI2 <- mask(LAI$IMAGE_2020.08.01_33UUP_22_LAI_10m_Id175305793_L2A,SCL$IMAGE_2020.08.01_33UUP_22_SCL_20m_Id175280181_L2A)
+
+# extract all Values of Raster Stack LAI which are no clouds
+# mask LAI Image with Cloud Mask
 
 
+
+### STEP 3: LAI Values Extraction - Linear Regression
 
 # 2.2. For-loop to extract LAI loop
 ## input:
